58edd23c4ad97a23fe5402917a1dfcbd2f269d4c
Thoi Dep Trai <thoi@deptrai.lhu>
2020-12-14 14:41:05 +0700

    Train new data

--- gen_txt.py
+++ gen_txt.py
@@ -4,16 +4,21 @@ from xml.dom.minidom import parse
 import cv2
 
 f = []
-root = './data'
+root = './data2'
 
 for (dirpath, dirnames, filenames) in walk(root + '/Annotations/'):
     f.append(filenames)
 data = f[0]
 np.random.shuffle(data)
 
-totalTrain = round(0.80*len(data))
+lenData = len(data)
+
+totalTrain = round(0.80*lenData)
+totalVal = round(0.19*lenData)
+
 train = data[:totalTrain]
-val = data[totalTrain:]
+val = data[totalTrain:totalTrain + totalVal]
+test = data[totalTrain + totalVal:]
 
 train_txt = open('train.txt', "w+")
 val_txt = open('val.txt', "w+")
@@ -25,25 +30,23 @@ def getFileNameWithoutExt(filename):
 
 
 def genDataset(dataset, file):
-    for i in range(len(dataset)):   
+    for i in range(len(dataset)):
         xml_data = parse(root + '/Annotations/' + dataset[i])
-        filename = xml_data.getElementsByTagName('filename')[0].firstChild.data      
-        print(root + "/JPEGImages/" + filename)  
+        filename = xml_data.getElementsByTagName('filename')[0].firstChild.data    
+        print("Checking " + root + "/JPEGImages/" + filename)    
         assert path.exists(root + "/JPEGImages/" + filename) == True
-        # assert len(xml_data.getElementsByTagName('object')) > 0    
-        # assert xml_data.getElementsByTagName('difficult')[0].firstChild.data == "0"
         file.write(getFileNameWithoutExt(filename) + "\n")
         trainval_txt.write(getFileNameWithoutExt(filename) + "\n")
         img = cv2.imread((root + "/JPEGImages/" + filename))
         assert img.shape == (240, 320, 3)
-        test_txt.write(getFileNameWithoutExt(filename) + "\n")
 
 print("Generating dataset...")
 
 genDataset(train, train_txt)
 genDataset(val, val_txt)
+genDataset(test, test_txt)
 
-print("Generated {} train and {} val".format(len(train), len(val)))
+print("Generated {} train, {} val and {} test".format(len(train), len(val), len(test)))
 print("Copying file...")
 train_txt.close()
 val_txt.close()
@@ -52,4 +55,4 @@ replace("train.txt", root + '/ImageSets/Main/train.txt')
 replace("val.txt", root + '/ImageSets/Main/val.txt')
 replace("trainval.txt", root + '/ImageSets/Main/trainval.txt')
 replace("test.txt", root + '/ImageSets/Main/test.txt')
-print(data)
+print("Done")
